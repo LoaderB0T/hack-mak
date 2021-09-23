68808adf186db42cf79137f503d22c9f4300ed86
Janik Schumacher <janik.schumacher@pmcs.de>
2021-09-23 13:35:06 +0200

    fixed

--- test1/projects/host/src/app/app-routes.ts
+++ test1/projects/host/src/app/app-routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 
+const URL = 'http://localhost:3000/remoteEntry.js';
+
 export const APP_ROUTES: Routes = [
     {
       path: 'module1',
-      loadChildren: () => import('module1/Module').then(m => m.FlightsModule)
+      loadChildren: () => import('module1/Module').then(m => m.AppModule)
     },
 ];
