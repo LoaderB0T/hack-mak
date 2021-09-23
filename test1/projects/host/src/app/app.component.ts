403ec747c553a5b1a50035df1ab7083d18b9b3b9
Janik Schumacher <janik.schumacher@pmcs.de>
2021-09-23 13:31:51 +0200

    .

--- test1/projects/host/src/app/app.component.ts
+++ test1/projects/host/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'host';
+
+  constructor(private readonly router: Router) {}
+
+  public nav() {
+    this.router.navigate(['/module1']);
+  }
 }
