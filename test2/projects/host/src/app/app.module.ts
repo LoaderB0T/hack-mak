c1bacc2aa4375a5f9fd5d9a096975c9eb43c7761
Martin Schmitz <Martin.Schmitz@helpline.pmcs.de>
2021-09-24 10:11:56 +0200

    add code bleu
    ~

--- test2/projects/host/src/app/app.module.ts
+++ test2/projects/host/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SWToolbarModule } from '@sw/angular-controls';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SWToolbarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
